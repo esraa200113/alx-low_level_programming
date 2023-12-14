2d6df2872f666fb37077ae3690d52cffa6dbbf39
esraa200113 <esraa200113@gmail.com>
2023-12-14 15:32:23 -0800

    Put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*Main is my entry point 
+*
+*Description : this is a c program that prints with the put function
+*
+*Return: Alwayes zero
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"); 	
+	return (0);
+}
