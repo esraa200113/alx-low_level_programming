94af0ab63bdf60ef67becb877489ba40cde30bb9
esraa200113 <esraa200113@gmail.com>
2023-12-14 16:27:36 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a piece of art with proper grammar using printf.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
