bbfcc3539a968783c361cf7af44b5ac4656a3dca
esraa200113 <esraa200113@gmail.com>
2023-12-15 16:31:54 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single-digit numbers of base 10
+ *              starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char digit = '0';
+
+	while (digit <= '9')
+		{
+			putchar(digit);
+			digit++;
+		}
+
+	putchar('\n');
+
+	return (0);
+}
+
