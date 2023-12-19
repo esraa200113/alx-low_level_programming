a827b8b3afec7ba1c0af0f6c2587b6ceb528c37d
esraa200113 <esraa200113@gmail.com>
2023-12-19 15:56:31 -0800

    abs of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: Takes in integer type input for the function.
+ *
+ * int:is integer input to the function.
+ *
+ * Return: The absolute value of @n.
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+
+}
