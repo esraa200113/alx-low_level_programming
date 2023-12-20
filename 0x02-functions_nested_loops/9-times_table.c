b2327a37130d93d435c8c852c8fa3817eadf6906
esraa200113 <esraa200113@gmail.com>
2023-12-20 06:34:00 -0800

    print table 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,10 +23,8 @@ void times_table(void)
 			if (product < 10)
 				_putchar(' ');
 			else
-			{
 				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
+			_putchar((product % 10) + '0');
 		}
 		}
 		_putchar('\n');
