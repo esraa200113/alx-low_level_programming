9c29762b5b3b228ef1dde0321183abbce2c2eef6
esraa200113 <esraa200113@gmail.com>
2024-01-02 16:58:54 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ * @s: Pointer to the string to be reversed.
+ */
+void rev_string(char *s)
+{
+	if (s != NULL)
+	{
+		int length = 0;
+		char *start = s;
+		char *end = s;
+		char temp;
+
+		while (*s != '\0')
+		{
+			length++;
+			s++;
+		}
+
+		if (length > 1)
+		{
+			end = start + length - 1;
+
+			while (start < end)
+			{
+				temp = *start;
+				*start = *end;
+				*end = temp;
+
+				start++;
+				end--;
+			}
+		}
+	}
+}
+
