c710bf03c3c888f87003fce0ee5faf3cd6e099f7
esraa200113 <esraa200113@gmail.com>
2024-01-02 17:44:44 -0800

    converts string into integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: Pointer to the input string.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+
+	if (s != NULL)
+	{
+		while (*s == ' ' || (*s >= 9 && *s <= 13))
+		s++;
+
+		while (*s == '-' || *s == '+')
+		{
+			if (*s == '-')
+			sign *= -1;
+			s++;
+		}
+
+		while (*s >= '0' && *s <= '9')
+		{
+			int digit = *s - '0';
+
+			if (result > (INT_MAX - digit) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+
+			result = result * 10 + digit;
+			s++;
+		}
+	}
+
+	return (result * sign);
+}
+
