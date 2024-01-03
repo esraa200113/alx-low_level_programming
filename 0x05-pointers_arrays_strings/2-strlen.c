742c3948c773cfbbf3dc1465d7c6b8d1c26899f2
esraa200113 <esraa200113@gmail.com>
2024-01-02 16:43:16 -0800

    program returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	if (s != NULL)
+	{
+		while (*s != '\0')
+		{
+			length++;
+			s++;
+		}
+	}
+
+	return (length);
+}
+
