aa2ffb12d3659a736a7070f14cd53052d9321ea0
esraa200113 <esraa200113@gmail.com>
2024-01-02 17:07:55 -0800

    program prints the every ither chars of strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first
+ *         character, followed by a new line.
+ * @str: Pointer to the string.
+ */
+void puts2(char *str)
+{
+	if (str != NULL)
+	{
+		while (*str != '\0')
+		{
+			_putchar(*str);
+			str += 2;
+		}
+
+		_putchar('\n');
+	}
+}
+
