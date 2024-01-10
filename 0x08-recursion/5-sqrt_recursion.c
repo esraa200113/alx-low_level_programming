e3040adf5331310504eaeaea1642aaf15cbf5cd8
esraa200113 <esraa200113@gmail.com>
2024-01-10 22:26:34 +0200

    print the square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,23 @@
  * @n: The number for which to calculate the square root.
  * Return: The square root if found, or -1 if n does not have square root.
  */
-
+int sqrt_recursive(int n, int guess)
+{
+	int square = guess * guess;
+	
+	if (square == n)
+	{
+		return (guess);
+	}
+	else if (square > n || guess > n / 2)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_recursive(n, guess + 1));
+	}
+}
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -14,6 +30,6 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		return (sqrt(n));
+		return (sqrt_recursive(n));
 	}
 }
