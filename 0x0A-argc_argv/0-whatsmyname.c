95350456669afd421592b3a88afe5fd248601702
esraa200113 <esraa200113@gmail.com>
2024-01-13 03:54:21 +0200

    argc and arvg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[]) 
+{
+	if (argc > 0) 
+	{
+		_puts(argv[0]);
+		return (0);
+	}
+	else 
+	{
+		_puts("Error: No command-line arguments.");
+		return (1);
+	}
+}
+
