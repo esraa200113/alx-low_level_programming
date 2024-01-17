de7d57db61e02123c959f08aa021fbf574b60178
esraa200113 <esraa200113@gmail.com>
2024-01-17 03:19:58 +0200

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,13 @@ char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 		return (NULL);
-	char *array = malloc(size * sizeof(char));
-
+	char *array;
+	unsigned int i;
+	array = malloc(size * sizeof(char));
+	
 	if (array == NULL)
 		return (NULL);
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
