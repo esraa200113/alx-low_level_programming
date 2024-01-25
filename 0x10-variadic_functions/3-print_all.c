c8dee9a8eaa47e14439aaf9f66dddefe795d5184
esraa200113 <esraa200113@gmail.com>
2024-01-25 21:25:54 +0200

    print all using variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,41 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *str;
+	char current_format;
+	
+	va_start(args, format);
+	while (format && format[i])
+	{
+		current_format = format[i];
+		if (i > 0)
+			printf(", ");
+		switch (current_format)
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
+
